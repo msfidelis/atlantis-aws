734670b761a308e19cdd2a1e97d4d90f87c78978
Matheus Fidelis <matheus@nanoshots.com.br>
2020-07-06 21:53:22 -0300

    foi

--- efs.tf
+++ efs.tf
@@ -7,9 +7,11 @@ resource "aws_efs_file_system" "main" {
 resource "aws_efs_mount_target" "efs_mount_target_a" {
   file_system_id = aws_efs_file_system.main.id
   subnet_id      = aws_subnet.public_subnet_1a.id
+  security_groups = [ aws_security_group.ec2_sg.id ]
 }
 
 resource "aws_efs_mount_target" "efs_mount_target_c" {
   file_system_id = aws_efs_file_system.main.id
   subnet_id      = aws_subnet.public_subnet_1c.id
+  security_groups = [ aws_security_group.ec2_sg.id ]
 }
